463a805e21a7bbb50b91f43ac60589f21fc49d87
Akamai98 <franbonino82@gmail.com>
2021-02-15 21:37:20 -0300

    v3.0 - let's start from scratch

--- src/FiestaDeFulbitoPackage/Position.java
+++
@@ -1,4 +0,0 @@
-package FiestaDeFulbitoPackage;
-
-//Enum que agrupa las posibles posiciones para los jugadores: COMODÍN, DEFENSORCENTRAL, DEFENSORLATERAL, MEDIOCAMPISTA O DELANTERO
-public enum Position { WILDCARD, CENTRALDEFENDER, LATERALDEFENDER, MIDFIELDER, FORWARD }
