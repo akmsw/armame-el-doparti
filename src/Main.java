846dfb6ac54d037b3564e0bff04817f52c3f3113
Akamai98 <franbonino82@gmail.com>
2021-02-15 22:02:09 -0300

    v3 - let's start from scratch

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+/**
+ * @author  Bonino, Francisco Ignacio.
+ * 
+ * @version 1.0.0
+ * 
+ * @since   15/02/2021
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+        //TODO.
+    }
+}
