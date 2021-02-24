48bd807197fc9d0337b0edd99437924e8e338496
Akamai98 <franbonino82@gmail.com>
2021-02-24 01:55:52 -0300

    regex approach, less hardcoding

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,6 @@
  * @since 15/02/2021
  */
 
-import Jama.Matrix;
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -16,24 +14,44 @@ import java.util.Scanner;
 
 public class Main {
 
+    //Campos privados.
     private static int playersAmount;
-    private static int centralDefendersAmount, lateralDefendersAmount, midfieldersAmount, forwardsAmount, wildcardsAmount;
     private static ArrayList<String> vector;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         Scanner monitorScan = new Scanner(System.in);
-
+        
         vector = new ArrayList<String>();
 
         System.out.print("Ingrese la cantidad de jugadores por equipo: ");
         
         playersAmount = monitorScan.nextInt();
 
-        printFileInfo(String.valueOf(playersAmount));
+        try {
+            printFileInfo(String.valueOf(playersAmount));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         monitorScan.close();
     }
 
+    /**
+     * Este método rescata la cantidad de jugadores para
+     * cada posición por equipo mediante expresiones regulares.
+     * 
+     * (C|L|M|F|W).>+.[0-9] :   Matchea las líneas que comiencen
+     *                          con C, L, M, F, ó W, estén seguidas
+     *                          por al menos un caracter >, y luego
+     *                          tengan algún número.
+     * 
+     * [A-Z].>+.            :   Matchea el trozo de la línea que
+     *                          no es un número.
+     * 
+     * @param   fileName    Nombre del archivo a buscar.
+     * 
+     * @throws IOException  Si el archivo no existe.
+     */
     private static void printFileInfo(String fileName) throws IOException {
         try (BufferedReader br = new BufferedReader(new FileReader("useful/FDF_F" + fileName + ".PDA"))) {
             String line;
@@ -41,18 +59,6 @@ public class Main {
             while ((line = br.readLine()) != null)
                 if (line.matches("(C|L|M|F|W).>+.[0-9]"))
                     vector.add(line.replaceAll("[A-Z].>+.", ""));
-            
-            centralDefendersAmount = Integer.parseInt(vector.get(0));
-            lateralDefendersAmount = Integer.parseInt(vector.get(1));
-            midfieldersAmount = Integer.parseInt(vector.get(2));
-            forwardsAmount = Integer.parseInt(vector.get(3));
-            wildcardsAmount = Integer.parseInt(vector.get(4));
-
-            System.out.println("CD: " + centralDefendersAmount);
-            System.out.println("LD: " + lateralDefendersAmount);
-            System.out.println("M: " + midfieldersAmount);
-            System.out.println("F: " + forwardsAmount);
-            System.out.println("W: " + wildcardsAmount);
          }
     }
 }
