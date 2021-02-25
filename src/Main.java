3378ca49d8f54d2c232a58e66fada57989b16fa5
Akamai98 <franbonino82@gmail.com>
2021-02-25 19:29:20 -0300

    format corrections

--- src/Main.java
+++ src/Main.java
@@ -9,31 +9,32 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
 
-    //Campos privados.
+    // Campos privados.
     private static ArrayList<String> data;
 
     public static void main(String[] args) {
         Scanner monitorScan = new Scanner(System.in);
-        
+
         data = new ArrayList<String>();
 
         HashMap<Position, Integer> playersMap = new HashMap<Position, Integer>();
 
         System.out.print("Ingrese la cantidad de jugadores por equipo: ");
-        
+
         int playersAmount = monitorScan.nextInt();
 
         try {
             collectData(String.valueOf(playersAmount));
 
             int index = 0;
-            
+
             for (Position position : Position.values()) {
                 playersMap.put(position, Integer.parseInt(data.get(index)));
                 index++;
@@ -44,26 +45,24 @@ public class Main {
             playersMap.forEach((key, value) -> System.out.println("POSICIÓN " + key + ": " + value));
         } catch (IOException e) {
             e.printStackTrace();
+            System.exit(-1);
         }
 
         monitorScan.close();
     }
 
     /**
-     * Este método rescata la cantidad de jugadores para
-     * cada posición por equipo mediante expresiones regulares.
+     * Este método rescata la cantidad de jugadores para cada posición por equipo
+     * mediante expresiones regulares.
      * 
-     * (C|L|M|F|W).>+.[0-9] :   Matchea las líneas que comiencen
-     *                          con C, L, M, F, ó W, estén seguidas
-     *                          por al menos un caracter >, y luego
-     *                          tengan algún número.
+     * (C|L|M|F|W).>+.[0-9] : Matchea las líneas que comiencen con C, L, M, F, ó W,
+     * estén seguidas por al menos un caracter >, y luego tengan algún número.
      * 
-     * [A-Z].>+.            :   Matchea el trozo de la línea que
-     *                          no es un número.
+     * [A-Z].>+. : Matchea el trozo de la línea que no es un número.
      * 
-     * @param   fileName    Nombre del archivo a buscar.
+     * @param fileName Nombre del archivo a buscar.
      * 
-     * @throws IOException  Si el archivo no existe.
+     * @throws IOException Si el archivo no existe.
      */
     private static void collectData(String fileName) throws IOException {
         try (BufferedReader br = new BufferedReader(new FileReader("useful/FDF_F" + fileName + ".PDA"))) {
@@ -72,6 +71,6 @@ public class Main {
             while ((line = br.readLine()) != null)
                 if (line.matches("(C|L|M|F|W).>+.[0-9]"))
                     data.add(line.replaceAll("[A-Z].>+.", ""));
-         }
+        }
     }
 }
