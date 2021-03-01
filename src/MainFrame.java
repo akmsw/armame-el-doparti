aa4a34b28ca241a084bc9d7fc8d38bfdbfe8fe15
Akamai98 <franbonino82@gmail.com>
2021-03-01 01:48:59 -0300

    finished MainFrame

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -16,7 +16,6 @@ import java.awt.event.WindowEvent;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JButton;
