61314ce7d1a4cb8ab0a2a8cdb9f6e7e281f7fd59
Akamai98 <franbonino82@gmail.com>
2021-03-01 22:28:17 -0300

    simplified Main class, regex search in MixFrame class

--- src/Main.java
+++ src/Main.java
@@ -6,24 +6,10 @@
  * @since 15/02/2021
  */
 
-/*import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-
-import java.util.ArrayList;
-import java.util.EnumMap;
-import java.util.Scanner;*/
-
 public class Main {
 
     // Constantes privadas.
     private static final String version = "v3.0";
-
-    // Campos privados.
-    /*private static ArrayList<String> data;
-    private static ArrayList<Player> CDSet, LDSet, MFSet, FWSet, WCSet;
-    private static EnumMap<Position, Integer> playersAmountMap;
-    private static Scanner monitorScan;*/
 
     /**
      * Método principal.
@@ -31,111 +17,6 @@ public class Main {
      * Aquí se instancia y ejecuta todo el programa.
      */
     public static void main(String[] args) {
-        /*monitorScan = new Scanner(System.in);
-
-        playersAmountMap = new EnumMap<>(Position.class);
-
-        data = new ArrayList<>();
-        CDSet = new ArrayList<>();
-        LDSet = new ArrayList<>();
-        MFSet = new ArrayList<>();
-        FWSet = new ArrayList<>();
-        WCSet = new ArrayList<>();
-
-        //System.out.print("Ingrese la cantidad de jugadores por equipo: ");
-
-        int playersAmount = monitorScan.nextInt();
-
-        try {
-            MyLogger log = new MyLogger("useful/DISTRO.PDA");
-
-            collectPDData(String.valueOf(playersAmount));
-
-            int index = 0;
-
-            for (Position position : Position.values()) {
-                playersAmountMap.put(position, Integer.parseInt(data.get(index)));
-                index++;
-            }
-
-            playersAmountMap.forEach((key, value) -> System.out.println("POSICIÓN " + key + ": " + value));
-
-            data.clear();
-
-            fillSet("defensor central", Position.CENTRALDEFENDER, CDSet);
-            fillSet("defensor lateral", Position.LATERALDEFENDER, LDSet);
-            fillSet("mediocampista", Position.MIDFIELDER, MFSet);
-            fillSet("delantero", Position.FORWARD, FWSet);
-            fillSet("comodín", Position.WILDCARD, WCSet);
-
-            printSet(Position.CENTRALDEFENDER, CDSet);
-            printSet(Position.LATERALDEFENDER, LDSet);
-            printSet(Position.MIDFIELDER, MFSet);
-            printSet(Position.FORWARD, FWSet);
-            printSet(Position.WILDCARD, WCSet);*/
-
-            MainFrame mainFrame = new MainFrame("Fiesta de Fulbito ", version);
-        /*} catch (IOException e) {
-            e.printStackTrace();
-            System.exit(-1);
-        }*/
-
-        //monitorScan.close();
+        MainFrame mainFrame = new MainFrame("Fiesta de Fulbito ", version);
     }
-
-    // ----------------------------------------Métodos privados---------------------------------
-
-    /**
-     * Este método rescata la cantidad de jugadores para cada posición por equipo
-     * mediante expresiones regulares.
-     * 
-     * [CLMFW].>+.[0-9] : Matchea las líneas que comiencen con C, L, M, F, ó W,
-     * estén seguidas por al menos un caracter >, y luego tengan algún número.
-     * 
-     * [A-Z].>+. : Matchea el trozo de la línea que no es un número.
-     * 
-     * @param   fileName    Nombre del archivo a buscar.
-     * 
-     * @throws  IOException Si el archivo no existe.
-     */
-    /*private static void collectPDData(String fileName) throws IOException {
-        try (BufferedReader br = new BufferedReader(new FileReader("useful/FDF_F" + fileName + ".PDA"))) {
-            String line;
-
-            while ((line = br.readLine()) != null)
-                if (line.matches("[CLMFW].>+.[0-9]"))
-                    data.add(line.replaceAll("[A-Z].>+.", ""));
-        }
-    }*/
-
-    /**
-     * Este método se encarga de rellenar un arreglo con un tipo de jugador específico.
-     * 
-     * @param   playerType  Posición del jugador (para mostrar al usuario).
-     * @param   position    Posición del jugador.
-     * @param   set         Arreglo a llenar.
-     */
-    /*private static void fillSet(String playerType, Position position, ArrayList<Player> set) {
-        String playerName;
-
-        for (int i = 0; i < playersAmountMap.get(position); i++) {
-            System.out.print("Ingrese el nombre del " + playerType + " #" + i + ": ");
-            
-            playerName = monitorScan.next();
-
-            set.add(new Player(playerName, position));
-        }
-    }*/
-
-    /**
-     * Este método se encarga de mostrar en pantalla un arreglo específico.
-     * 
-     * @param   position    Tipo de jugador.
-     * @param   set         Arreglo a mostrar.
-     */
-    /*private static void printSet(Position position, ArrayList<Player> set) {
-        for (Player player : set) {
-            System.out.println("JUGADOR " + position + ": " + player.getName());
-        }
-    }*/
 }
--- src/MixFrame.java
+++ src/MixFrame.java
@@ -6,11 +6,19 @@
  * @since 28/02/2021
  */
 
+import java.util.ArrayList;
+import java.util.EnumMap;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
+import javax.sound.midi.SysexMessage;
 import javax.swing.ImageIcon;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
@@ -30,6 +38,8 @@ public class MixFrame extends JFrame implements ActionListener {
     private ImageIcon icon;
     private JPanel panel;
     private JComboBox<String> comboBox;
+    private static ArrayList<String> data;
+    private static EnumMap<Position, Integer> playersAmountMap;
 
     /**
      * Constructor. Aquí se crea la ventana de mezcla.
@@ -38,12 +48,57 @@ public class MixFrame extends JFrame implements ActionListener {
         this.playersAmount = playersAmount;
         this.icon = icon;
 
+        playersAmountMap = new EnumMap<>(Position.class);
+
+        data = new ArrayList<>();
+
+        try {
+            collectPDData(String.valueOf(playersAmount));
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
+
+        int index = 0;
+
+        for (Position position : Position.values()) {
+            playersAmountMap.put(position, Integer.parseInt(data.get(index)));
+            index++;
+        }
+
+        playersAmountMap.forEach((key, value) -> System.out.println("POSICIÓN " + key + ": " + value));
+
+        data.clear();
+
         initializeComponents("Ingreso de jugadores - Fútbol " + playersAmount);
 
         setVisible(true);
     }
 
     // ----------------------------------------Métodos privados---------------------------------
+
+    /**
+     * Este método rescata la cantidad de jugadores para cada posición por equipo
+     * mediante expresiones regulares.
+     * 
+     * [CLMFW].>+.[0-9] : Matchea las líneas que comiencen con C, L, M, F, ó W,
+     * estén seguidas por al menos un caracter >, y luego tengan algún número.
+     * 
+     * [A-Z].>+. : Matchea el trozo de la línea que no es un número.
+     * 
+     * @param   fileName    Nombre del archivo a buscar.
+     * 
+     * @throws  IOException Si el archivo no existe.
+     */
+    private static void collectPDData(String fileName) throws IOException {
+        try (BufferedReader br = new BufferedReader(new FileReader("useful/FDF_F" + fileName + ".PDA"))) {
+            String line;
+
+            while ((line = br.readLine()) != null)
+                if (line.matches("[CLMFW].>+.[0-9]"))
+                    data.add(line.replaceAll("[A-Z].>+.", ""));
+        }
+    }
 
     /**
      * Este método se encarga de inicializar los componentes de la ventana de
