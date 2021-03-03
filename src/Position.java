7f2074451405f2fc877fb3b17d0266e43add62d9
Akamai98 <franbonino82@gmail.com>
2021-03-03 00:09:51 -0300

    fixed documentation

--- src/Position.java
+++ src/Position.java
@@ -1,4 +1,7 @@
 /**
+ * Enum con las posibles posiciones para un jugador. Se excluye la posición de
+ * arquero y es reemplazada por 'WILDCARD' por pedido del cliente.
+ * 
  * @author  Bonino, Francisco Ignacio.
  * 
  * @version 1.0.0
@@ -6,10 +9,6 @@
  * @since   15/02/2020
  */
 
-/**
- * Enum con las posibles posiciones para un jugador. Se excluye la posición de
- * arquero y es reemplazada por 'WILDCARD' por pedido del cliente.
- */
 public enum Position {
     CENTRALDEFENDER, LATERALDEFENDER, MIDFIELDER, FORWARD, WILDCARD
 }
