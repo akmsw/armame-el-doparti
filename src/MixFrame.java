be1bf300f220ba1621b78f75c1df049d2c14c6be
Akamai98 <franbonino82@gmail.com>
2021-03-03 00:07:23 -0300

    changed frame color

--- src/MixFrame.java
+++ src/MixFrame.java
@@ -12,6 +12,7 @@ import java.util.EnumMap;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.FocusAdapter;
@@ -147,6 +148,8 @@ public class MixFrame extends JFrame implements ActionListener {
         addButtons();
 
         addTextArea();
+
+        panel.setBackground(new Color(200, 200, 200));
 
         add(panel);
     }
