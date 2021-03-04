084a68dbbbfa6c402ebec1603e8a09a788eab879
Akamai98 <franbonino82@gmail.com>
2021-03-03 22:11:23 -0300

    removed unused imports

--- src/MixFrame.java
+++ src/MixFrame.java
@@ -28,7 +28,6 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseAdapter;
 
 import javax.swing.BorderFactory;
-import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
