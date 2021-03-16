af457f31e329cd99076951734e031e0080eef2d1
Akamai98 <franbonino82@gmail.com>
2021-03-16 15:59:40 -0300

    removed unnecesary imports

--- src/AnchorageFrame.java
+++ src/AnchorageFrame.java
@@ -13,7 +13,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import java.awt.Color;
-import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
