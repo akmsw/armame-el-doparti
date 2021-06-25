ba112e06eaa1bd9953ac60512fbcdff67488054c
Fran Bonino <franbonino82@gmail.com>
2021-06-25 02:24:09 -0300

    nothing important

--- src/RatingFrame.java
+++ src/RatingFrame.java
@@ -43,7 +43,7 @@ public class RatingFrame extends JFrame {
         finishButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                // TODO Auto-generated method stub
+                // TODO.
             }
         });
 
