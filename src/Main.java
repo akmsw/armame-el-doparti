22144498f28e54d503d7e6414fed141e00735b4d
Fran Bonino <franbonino82@gmail.com>
2021-06-27 00:42:22 -0300

    changed app font

--- src/Main.java
+++ src/Main.java
@@ -9,10 +9,13 @@
  */
 
 import java.awt.Color;
+import java.awt.Font;
 
 import java.util.ArrayList;
+import java.util.Enumeration;
 
 import javax.swing.UIManager;
+import javax.swing.plaf.FontUIResource;
 
 public class Main {
 
@@ -26,7 +29,8 @@ public class Main {
     public static ArrayList<String> positions; // Arreglo con los strings de las posiciones de los jugadores.
 
     // Constantes privadas.
-    public static final Color SELECT_NOTIF_FRAME_BG_COLOR = new Color(176, 189, 162); // Color de fondo de las ventanas de selección/notificación.
+    private static final Color SELECT_NOTIF_FRAME_BG_COLOR = new Color(176, 189, 162); // Color de fondo de las ventanas de selección/notificación.
+    private static final FontUIResource PROGRAM_FONT = new FontUIResource("Verdana", Font.PLAIN, 11); // Fuente utilizada para el programa.
     
     /**
      * Método principal.
@@ -45,8 +49,28 @@ public class Main {
         positions.add("DELANTEROS");
         positions.add("COMODINES");
 
+        setUIFont(PROGRAM_FONT);
+
         MainFrame mainFrame = new MainFrame();
 
         mainFrame.setVisible(true);
+    }
+
+    /**
+     * Este método se encarga de setear la fuente utilizada para el programa.
+     * 
+     * @param f Fuente a utilizar. Debe estar incluida en el paquete standard
+     *          del sistema operativo Windows.
+     */
+    public static void setUIFont (FontUIResource f) {
+        Enumeration<Object> keys = UIManager.getDefaults().keys();
+
+        while (keys.hasMoreElements()) {
+            Object key = keys.nextElement();
+            Object value = UIManager.get(key);
+
+            if (value instanceof FontUIResource)
+                UIManager.put(key, f);
+        }
     }
 }
