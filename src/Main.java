0dca654e58484cb74c55a99c15b72861574e0a06
Fran Bonino <franbonino82@gmail.com>
2021-06-27 00:08:54 -0300

    nothing important

--- src/Main.java
+++ src/Main.java
@@ -21,10 +21,12 @@ public class Main {
     public static final String PROGRAM_VERSION = "v3.0";
     public static final String IMG_PATH = "src/graphics/"; // Carpeta donde buscar las imágenes.
     public static final Color FRAMES_BG_COLOR = new Color(176, 189, 162); // Color de fondo de las ventanas.
+
+    // Campos públicos.
     public static ArrayList<String> positions; // Arreglo con los strings de las posiciones de los jugadores.
 
     // Constantes privadas.
-    public static final Color OPTIONPANE_BG_COLOR = new Color(176, 189, 162); // Color de fondo de las ventanas.
+    public static final Color SELECT_NOTIF_FRAME_BG_COLOR = new Color(176, 189, 162); // Color de fondo de las ventanas de selección/notificación.
     
     /**
      * Método principal.
@@ -32,8 +34,8 @@ public class Main {
      * Aquí se instancia y ejecuta todo el programa.
      */
     public static void main(String[] args) {        
-        UIManager.put("OptionPane.background", OPTIONPANE_BG_COLOR);
-        UIManager.put("Panel.background", OPTIONPANE_BG_COLOR);
+        UIManager.put("OptionPane.background", SELECT_NOTIF_FRAME_BG_COLOR);
+        UIManager.put("Panel.background", SELECT_NOTIF_FRAME_BG_COLOR);
 
         positions = new ArrayList<>();
 
