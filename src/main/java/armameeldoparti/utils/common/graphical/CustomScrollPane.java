1abbd308ff8982bd0c2fb775de9e5275fff6260c
akmsw <franbonino82@gmail.com>
2023-07-06 23:47:03 -0300

    fixed scrollbar buttons aesthetics

--- src/main/java/armameeldoparti/utils/common/graphical/CustomScrollPane.java
+++ src/main/java/armameeldoparti/utils/common/graphical/CustomScrollPane.java
@@ -1,9 +1,13 @@
 package armameeldoparti.utils.common.graphical;
 
 import armameeldoparti.utils.common.Constants;
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.ScrollPaneConstants;
+import javax.swing.border.BevelBorder;
+import javax.swing.plaf.basic.BasicArrowButton;
 import javax.swing.plaf.basic.BasicScrollBarUI;
 import lombok.Getter;
 
@@ -43,16 +47,58 @@ public class CustomScrollPane extends JScrollPane {
 
   /**
    * Configures the graphical properties of the scroll pane in order to fit the program aesthetics.
-   *
-   * <p>The thumb will be dark green and the track will be medium green.
    */
   private void setupGraphicalProperties() {
     setBackground(Constants.GREEN_LIGHT);
     getVerticalScrollBar().setUI(new BasicScrollBarUI() {
+      /**
+       * Configures the scrollbar colors to fit the program aesthetics.
+       *
+       * <p>The thumb will be dark green and the track will be medium green.
+       */
       @Override
       protected void configureScrollBarColors() {
         this.thumbColor = Constants.GREEN_DARK;
         this.trackColor = Constants.GREEN_MEDIUM;
+      }
+
+      /**
+       * Configures the scrollbar decrease button to fit the program aesthetics.
+       *
+       * <p>The button and the shadows will be medium green. The arrow will be light green.
+       *
+       * @param orientation The button arrow orientation.
+       *
+       * @return The scrollbar decrease button.
+       */
+      @Override
+      protected JButton createDecreaseButton(int orientation) {
+        JButton decreaseButton = new BasicArrowButton(orientation,
+                                                      Constants.GREEN_MEDIUM,
+                                                      Constants.GREEN_MEDIUM,
+                                                      Constants.GREEN_LIGHT,
+                                                      Constants.GREEN_MEDIUM);
+
+        decreaseButton.setBorder(BorderFactory.createBevelBorder(BevelBorder.RAISED,
+                                                                 Constants.GREEN_MEDIUM,
+                                                                 Constants.GREEN_DARK));
+
+        return decreaseButton;
+      }
+
+      /**
+       * Configures the scrollbar increase button to fit the program aesthetics. It'll have the same
+       * graphical properties as the scrollbar decrease button.
+       *
+       * @see #createDecreaseButton(int)
+       *
+       * @param orientation The button arrow orientation.
+       *
+       * @return The scrollbar increase button.
+       */
+      @Override
+      protected JButton createIncreaseButton(int orientation) {
+        return createDecreaseButton(orientation);
       }
     });
   }
