97acd4992d3cd0ec7715f0082d8747454cfe4604
akmsw <franbonino82@gmail.com>
2023-07-09 18:29:43 -0300

    documentation fix

--- src/main/java/armameeldoparti/utils/common/custom/graphical/CustomTextArea.java
+++ src/main/java/armameeldoparti/utils/common/custom/graphical/CustomTextArea.java
@@ -9,7 +9,7 @@ import javax.swing.border.BevelBorder;
  * Custom text area class.
  *
  * <p>This class is used to instantiate a custom text area that fits the overall program
- * aesthetics.
+ * aesthetics. It is supposed to be used along with a CustomScrollPane if a scrollbar is needed.
  *
  * @author Bonino, Francisco Ignacio.
  *
@@ -24,8 +24,9 @@ public class CustomTextArea extends JTextArea {
   /**
    * Builds a basic text area using the established program aesthetics.
    *
-   * @param rows    Row count for the text area.
-   * @param columns Column count for the text area.
+   * @param rows       Row count for the text area.
+   * @param columns    Column count for the text area.
+   * @param scrollable If the text area could handle a text long enough to have a scrollbar.
    */
   public CustomTextArea(int rows, int columns, boolean scrollable) {
     super(rows, columns);
@@ -44,6 +45,9 @@ public class CustomTextArea extends JTextArea {
 
   /**
    * Configures the graphical properties for the text area in order to fit the program aesthetics.
+   *
+   * @param scrollable If the text area could handle a text long enough to have a scrollbar. If so,
+   *                   no border is set for the text area. If not, a bevel border is set.
    */
   private void setupGraphicalProperties(boolean scrollable) {
     setEditable(false);
