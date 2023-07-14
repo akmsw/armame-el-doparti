ca59a23a419df976d78dab2383b36386ee1d5af2
akmsw <franbonino82@gmail.com>
2023-07-14 00:12:38 -0300

    removed unused import

--- src/main/java/armameeldoparti/views/MainMenuView.java
+++ src/main/java/armameeldoparti/views/MainMenuView.java
@@ -6,7 +6,6 @@ import armameeldoparti.utils.common.CommonFunctions;
 import armameeldoparti.utils.common.Constants;
 import java.awt.Color;
 import java.awt.Font;
-import java.awt.Image;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
