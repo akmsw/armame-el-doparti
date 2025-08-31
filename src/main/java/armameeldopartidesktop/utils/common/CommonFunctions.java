ccb773934eda005249586fc5862e244d551d26a0
akmsw <franbonino82@gmail.com>
2025-08-31 19:23:29 -0300

    remove extra space

--- src/main/java/armameeldopartidesktop/utils/common/CommonFunctions.java
+++ src/main/java/armameeldopartidesktop/utils/common/CommonFunctions.java
@@ -79,7 +79,7 @@ public final class CommonFunctions {
       dumpFile.write("Player limit per position:" + System.lineSeparator());
       dumpFile.write("\t" + CommonFields.getPlayerLimitPerPosition().entrySet().toString() + System.lineSeparator().repeat(2));
       dumpFile.write("Positions map:" + System.lineSeparator());
-      dumpFile.write("\t" + Constants.MAP_POSITIONS.entrySet().toString() + System.lineSeparator() .repeat(2));
+      dumpFile.write("\t" + Constants.MAP_POSITIONS.entrySet().toString() + System.lineSeparator().repeat(2));
       dumpFile.write("Controllers map:" + System.lineSeparator());
       dumpFile.write("\t" + CommonFields.getControllersMap().entrySet().toString() + System.lineSeparator().repeat(2));
       dumpFile.write("Players:" + System.lineSeparator().repeat(2));
