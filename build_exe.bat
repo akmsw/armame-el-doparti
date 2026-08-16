89c0c2bf52a015699d20573c75d4df079c69327a
akmsw <franbonino82@gmail.com>
2026-08-16 12:07:07 -0300

    add build exe bat

---
+++ build_exe.bat
@@ -0,0 +1,11 @@
+@echo off
+
+jpackage ^
+  --input .\target ^
+  --name armame-el-doparti-3.1.0 ^
+  --main-jar armameeldoparti-3.1.0.jar ^
+  --main-class armameeldopartidesktop.Main ^
+  --type app-image ^
+  --icon ".\src\main\res\img\icons\main_icon.ico"
+
+pause
