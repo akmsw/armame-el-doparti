45abfc5e2866f33037f82d477e2d1f6cead2fb2e
akmsw <franbonino82@gmail.com>
2026-08-17 21:27:38 -0300

    enhance cardlayout behavior

--- src/main/java/armameeldopartidesktop/utils/common/CommonFields.java
+++ src/main/java/armameeldopartidesktop/utils/common/CommonFields.java
@@ -39,6 +39,7 @@ public final class CommonFields {
 
   private static Map<Position, Integer> playerLimitPerPosition;
   private static Map<Position, List<Player>> playersSets;
+  private static Map<ProgramView, View> viewsMap;
   private static Map<ProgramView, Controller<? extends View>> controllersMap;
 
   // ---------- Constructor -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -78,6 +79,10 @@ public final class CommonFields {
 
   public static Map<Position, List<Player>> getPlayersSets() {
     return playersSets;
+  }
+
+  public static Map<ProgramView, View> getViewsMap() {
+    return viewsMap;
   }
 
   public static Map<ProgramView, Controller<? extends View>> getControllersMap() {
